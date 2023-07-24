02e63eefbe85354aab18112f6418d979ba7f7a69
Yuvansarathy <139550429+Yuvansarathy@users.noreply.github.com>
2023-07-24 14:43:07 +0530

    Add files via upload

---
+++ tstsPerson.java
@@ -0,0 +1,12 @@
+package day2_programs;
+
+public class Person {
+
+	public static void main(String[] args) {
+		PersonDemo p1=new PersonDemo();
+		p1.name="singhh";
+		System.out.println(p1.name);
+
+	}
+
+}
